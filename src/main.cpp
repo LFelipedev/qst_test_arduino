4ef4f7b74ddb568def87c946eb3163a210d4901d
LFelipedev <luiz.fma.up@gmail.com>
2025-05-29 19:23:39 -0300

    Implementação básica do freeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,95 @@
 #include <Arduino.h>
+
+#include "Arduino_FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const int PINO_ONEWIRE = 12; // Define pino do sensor
-OneWire oneWire(PINO_ONEWIRE); // Cria um objeto OneWire
-DallasTemperature sensor(&oneWire); // Informa a referencia da biblioteca dallas temperature para Biblioteca onewire
-DeviceAddress endereco_temp; // Cria um endereco temporario da leitura do sensor
+const int sensor_temperatura = 12; 
+const int potenciometro = A0;
+
+TaskHandle_t potenciometroTaskH;
+TaskHandle_t temperaturaTaskH;
+
+const int send_gantt = 1;
+uint16_t valor_potenciometro; 
+float temperatura_c;
+
+SemaphoreHandle_t SerialMutex;
+
+OneWire oneWire(sensor_temperatura); 
+DallasTemperature sensor(&oneWire); 
+DeviceAddress endereco_temp;
+
+void sendGantt(const char *name, unsigned int stime, unsigned int etime);
+void potenciometroTask(void *arg);
+void temperaturaTask(void *arg);
 
 void setup() {
-  Serial.begin(9600); // Inicia a porta serial
-  Serial.println("Medindo Temperatura"); // Imprime a mensagem inicial
-  sensor.begin(); ; // Inicia o sensor
+  Serial.begin(9600); 
+  sensor.begin();
+
+  SerialMutex = xSemaphoreCreateMutex();
+  
+  xTaskCreate(temperaturaTask,            
+              "temperaturaTask",          
+              256,                
+              NULL,               
+              1,                  
+              &temperaturaTaskH);
+  
+  xTaskCreate(potenciometroTask,
+              "potenciometroTask",
+              128,
+              NULL,
+              0,
+              &potenciometroTaskH);
 }
   
 void loop() {
-  sensor.requestTemperatures(); // Envia comando para realizar a conversão de temperatura
-  if (!sensor.getAddress(endereco_temp,0)) { // Encontra o endereco do sensor no barramento
-    Serial.println("SENSOR NAO CONECTADO"); // Sensor conectado, imprime mensagem de erro
-  } else {
-    Serial.print("Temperatura = "); // Imprime a temperatura no monitor serial
-    Serial.println(sensor.getTempC(endereco_temp), 1); // Busca temperatura para dispositivo
+}
+
+void sendGantt(const char *name, unsigned int stime, unsigned int etime) {
+    if(xSemaphoreTake(SerialMutex, portMAX_DELAY) == pdTRUE) {  
+        Serial.print("\t\t");
+        Serial.print(name);
+        Serial.print(": ");
+        Serial.print(stime);
+        Serial.print(", ");
+        Serial.println(etime);
+        xSemaphoreGive(SerialMutex);                           
+    }
+}
+
+void potenciometroTask(void *arg) {
+
+  while(true) {
+    valor_potenciometro = analogRead(potenciometro);
+    Serial.print("Potenciometro: ");
+    Serial.println(valor_potenciometro);
+    
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
-  delay(100);
+}
+
+void temperaturaTask(void *arg) {
+
+    while (true) {
+
+      sensor.requestTemperatures();
+
+      if(sensor.getAddress(endereco_temp, 0)) {
+        temperatura_c = sensor.getTempC(endereco_temp);
+        Serial.print("Temperatura: ");
+        Serial.println(temperatura_c);
+      } 
+      
+      else {
+        Serial.println("Erro ao tentar ler o sensor");
+      }
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
 }
